f903e8fce1422781b9784b0fb8735042531aae67
Jorge Martín <jorgem@element.io>
2023-11-28 10:55:06 +0100

    Bump version, set as snapshot

--- compound/build.gradle.kts
+++ compound/build.gradle.kts
@@ -85,7 +85,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.S01)
     signAllPublications()
 
-    coordinates("io.element.android", "compound-android", "0.0.2")
+    coordinates("io.element.android", "compound-android", "0.0.3-SNAPSHOT")
     if (!providers.gradleProperty("mavenCentralUsername").isPresent) {
         println("No maven central provider")
     }
