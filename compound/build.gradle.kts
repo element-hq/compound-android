5d7919972f63258030b7c856508fb2372660f55b
Jorge Martín <jorgem@element.io>
2023-12-29 09:37:20 +0100

    Version `0.0.3`

--- compound/build.gradle.kts
+++ compound/build.gradle.kts
@@ -90,7 +90,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.S01)
     signAllPublications()
 
-    coordinates("io.element.android", "compound-android", "0.0.3-SNAPSHOT")
+    coordinates("io.element.android", "compound-android", "0.0.3")
     if (!providers.gradleProperty("mavenCentralUsername").isPresent) {
         println("No maven central provider")
     }
