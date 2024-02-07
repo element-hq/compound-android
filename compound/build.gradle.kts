344c5a0824a1b064d22a4258a5864a2cd44e4cc9
Jorge Martín <jorgem@element.io>
2024-02-07 15:28:41 +0100

    Bump version: 0.0.4

--- compound/build.gradle.kts
+++ compound/build.gradle.kts
@@ -90,7 +90,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.S01)
     signAllPublications()
 
-    coordinates("io.element.android", "compound-android", "0.0.4-SNAPSHOT")
+    coordinates("io.element.android", "compound-android", "0.0.4")
     if (!providers.gradleProperty("mavenCentralUsername").isPresent) {
         println("No maven central provider")
     }
