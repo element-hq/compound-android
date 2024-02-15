f96788ccd92941da3716ac9ff1fe4ab9cde646ba
Jorge Martín <jorgem@element.io>
2024-02-15 11:11:58 +0100

    Try fixing nightly reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,28 @@ buildscript {
 plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.sonarqube)
+    alias(libs.plugins.dependencycheck)
+}
+
+// To run a sonar analysis:
+// Run './gradlew sonar -Dsonar.login=<SONAR_LOGIN>'
+// The SONAR_LOGIN is stored in passbolt as Token Sonar Cloud Bma
+// Sonar result can be found here: https://sonarcloud.io/project/overview?id=element-hq_compound-android
+sonar {
+    properties {
+        property("sonar.projectName", "compound-android")
+        property("sonar.projectKey", "element-hq_compound-android")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.projectVersion", "1.0")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.links.homepage", "https://github.com/element-hq/compound-android/")
+        property("sonar.links.ci", "https://github.com/element-hq/compound-android/actions")
+        property("sonar.links.scm", "https://github.com/element-hq/compound-android/")
+        property("sonar.links.issue", "https://github.com/element-hq/compound-android/issues")
+        property("sonar.organization", "new_vector_ltd_organization")
+        property("sonar.login", if (project.hasProperty("SONAR_LOGIN")) project.property("SONAR_LOGIN")!! else "invalid")
+
+        // exclude source code from analyses separated by a colon (:)
+    }
 }
