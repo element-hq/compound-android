745afcd28cae75644c6814aa48087e16d84d0e5d
Benoit Marty <benoit@matrix.org>
2025-02-03 15:11:02 +0100

    Add missing copyright headers

--- compound/src/main/kotlin/io/element/android/compound/theme/AvatarColors.kt
+++ compound/src/main/kotlin/io/element/android/compound/theme/AvatarColors.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2023, 2025 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
 package io.element.android.compound.theme
 
 import androidx.compose.foundation.background
